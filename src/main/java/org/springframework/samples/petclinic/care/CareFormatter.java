c3fae355da371e1460962e4f6100077dc806e895
Juan Romero <juaromgon6@alum.us.es>
2022-12-22 12:38:46 +0100

    Test 5

--- src/main/java/org/springframework/samples/petclinic/care/CareFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareFormatter.java
@@ -3,22 +3,28 @@ package org.springframework.samples.petclinic.care;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CareFormatter implements Formatter<Care>{
 
+    @Autowired
+    private CareService careService;
+
     @Override
     public String print(Care object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+        return object.name;
     }
 
     @Override
     public Care parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+        Care cr = careService.getCare(text);
+        if (cr == null) {
+            throw new ParseException("Product type not found: " + text, 0);
+        }
+        return cr;
     }
     
 }
